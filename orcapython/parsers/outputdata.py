d64bbee6876ce5ce7c48390634ecc0d2b7e03379
oboril <jan.oboril@gmail.com>
2023-07-29 12:16:47 +0100

    changed outputdata summary

--- orcapython/parsers/outputdata.py
+++ orcapython/parsers/outputdata.py
@@ -79,6 +79,15 @@ class OutputData:
         
         if self.final_energy is not None:
             text += f"Final energy: {self.final_energy:0.1f} kJ/mol ({len(self.single_point_energies)} S.P. energies logged)\n"
+
+        # if orca did not terminate normally, print also all single point energies:
+        if len(self.single_point_energies) > 2:
+            tmpl = "{: <10}{: >20}{: >20}"
+            print(tmpl.format("Step","Energy [kJ/mol]","Difference [kJ/mol]"))
+            prev_eng = 0
+            for idx, energ in enumerate(self.single_point_energies):
+                print(tmpl.format(idx, f"{energ:0.1f}", f"{energ - prev_eng:0.1f}"))
+                prev_eng = energ
         
         if len(self.vib_freqs) > 0:
             if not self.negative_freqs:
